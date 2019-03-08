4a52e12f75d8292ac99932d6111deba86c3eb7c7
vipulsinghbrahmania <vipulsinghbrahmania@gmail.com>
2019-03-08 22:28:04 +0530

    modified the first condition of the loop to build query

--- src/stats/views.py
+++ src/stats/views.py
@@ -12,32 +12,33 @@ def home(request):
 	more_7 = 0
 	count = 1
 	total = 0
-	query = request.POST.get('query')
 	
-	while True:
-		# result limited by API to 100 per page.
-		url = 'https://api.github.com/repos/' + query[19:] + '/issues?page=' + str(count) + '&per_page=100'
-		res = requests.get(url).json()
-		
-		if len(res) == 0: # break when no more pages.
-			break
+	query = request.POST.get('query')
+	if query:
+		while True:
+			# results limited by API to 100 per page.
+			url = 'https://api.github.com/repos/' + query[19:] + '/issues?page=' + str(count) + '&per_page=100'
+			res = requests.get(url).json()
+			
+			if len(res) == 0: # break when no more pages.
+				break
 
-		else:
-			count += 1
-			total += len(res) # count of total issues.
-			for i in res:
-				x = datetime.strptime(i['created_at'], '%Y-%m-%dT%H:%M:%SZ') # creation time of the issue.
-				y = datetime.strptime(str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f') # now time.
-				z = (y-x).total_seconds()
-				
-				if z < 86400: # last 24 hours.
-					last_24 += 1
-				
-				elif z > 86400 and z < 604800: # more than 24 hours and less than a week.
-					seven_24 += 1
-				
-				else: # more than a week.
-					more_7 += 1
+			else:
+				count += 1
+				total += len(res) # count of total issues.
+				for i in res:
+					x = datetime.strptime(i['created_at'], '%Y-%m-%dT%H:%M:%SZ') # creation time of the issue.
+					y = datetime.strptime(str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f') # now time.
+					z = (y-x).total_seconds()
+					
+					if z < 86400: # last 24 hours.
+						last_24 += 1
+					
+					elif z > 86400 and z < 604800: # more than 24 hours and less than a week.
+						seven_24 += 1
+					
+					else: # more than a week.
+						more_7 += 1
 
 	result = {
 		'total': total,
